f287da1b56105ec9c38a5657064956798fce1365
navoni1024 <radec8402@gmail.com>
2024-01-12 17:16:22 +0800

    fix weather foacast

--- core/cwb.py
+++ core/cwb.py
@@ -5,16 +5,37 @@ from discord.ext import commands
 import requests
 
 def cwb(location):
-	with open('weather.json','r',encoding='utf8') as jfile:
-			jdata = json.load(jfile)
-	try:
-		url = jdata[location]
-	except:
-		return "dafaq"
+	url = "https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/F-C0032-005?Authorization=CWB-8AA5A698-DA03-45A2-81E9-F45E7A8EABEF&downloadType=WEB&format=JSON"
 	orgData = requests.get(url)
 	data = orgData.json()
-	forecastList =  data['cwbopendata']['dataset']['parameterSet']['parameter']
-	f = ''
+
+	if(location == "台北市"):
+		location="臺北市"
+	if(location == "台北縣" or location == "臺北縣"):
+		location="新北市"
+
+	forecastList = data["cwaopendata"]["dataset"]["location"]
 	for i in forecastList:
-		f = f+i['parameterValue']
-	return f
+		if(i["locationName"]==location):
+			forecastList = i
+			break
+	
+	try:
+		tmp = forecastList["locationName"]
+	except:
+		return "dafaq"
+	
+	
+	result="**"+forecastList["locationName"]
+	result+= '**\n'
+	result+= forecastList["weatherElement"][0]["time"][2]["startTime"][:10]+' '
+	result+= forecastList["weatherElement"][0]["time"][2]["startTime"][11:16]
+	result+= ' ~ '
+	result+= forecastList["weatherElement"][0]["time"][2]["endTime"][:10]+' '
+	result+= forecastList["weatherElement"][0]["time"][2]["endTime"][11:16]
+	result+= '\n'
+	result+= forecastList["weatherElement"][0]["time"][2]["parameter"]["parameterName"]+' '
+	result+= forecastList["weatherElement"][1]["time"][2]["parameter"]["parameterName"]+"°C "
+
+	return result
+
