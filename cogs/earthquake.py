fdb5bc926f8080e12605d80c3dfaa54755d99533
navoni1024 <radec8402@gmail.com>
2024-10-27 15:28:11 +0800

    fix earthquake

--- cogs/earthquake.py
+++ cogs/earthquake.py
@@ -43,7 +43,7 @@ async def send_quack_info(QuackData, QuackCh):
         os.close(temp_file[0])
         os.unlink(temp_file[1])
     
-    if("嘉義" in QuackData["EarthquakeInfo"]["Epicenter"]["Location"]):    
+    elif("嘉義" in QuackData["EarthquakeInfo"]["Epicenter"]["Location"]):    
         temp_file = tempfile.mkstemp(suffix=".png")
         image_composite(ReprotImage, os.path.join(PicDict, "cha.png"), temp_file[1])
         ReprotImage = discord.File(temp_file[1])
