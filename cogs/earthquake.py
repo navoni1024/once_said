6dbe57f3e2e896eae35ee7c18cd0e20b179165bf
navoni1024 <radec8402@gmail.com>
2025-05-05 18:31:58 +0800

    add tainan

--- cogs/earthquake.py
+++ cogs/earthquake.py
@@ -82,6 +82,16 @@ async def send_quack_info(QuackData, QuackCh):
         os.close(temp_file[0])
         os.unlink(temp_file[1])
 
+    elif("臺南" in QuackData["EarthquakeInfo"]["Epicenter"]["Location"]):    
+        temp_file = tempfile.mkstemp(suffix=".png")
+        image_composite(ReprotImage, os.path.join(PicDict, "melon.png"), temp_file[1])
+        ReprotImage = discord.File(temp_file[1])
+        
+        await QuackCh.send(info)
+        await QuackCh.send(file=ReprotImage)
+        os.close(temp_file[0])
+        os.unlink(temp_file[1])   
+
     else:
         temp_file = tempfile.mkstemp(suffix=".png")
         ReprotImage.save(temp_file[1])
